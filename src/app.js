2d3c66b8218325343064616fe53b16e575186215
lexsaso <jalexanderssoto@hotmail.com>
2026-06-17 18:29:10 +0000

    juego serie

--- src/app.js
+++ src/app.js
@@ -1,3 +1,41 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+const opciones = ["rock", "paper", "scissors", "lizard", "spock"];
 
+// El mapa de reglas 
+const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"]
+};
+
+const jugar = (userChoice) => {
+
+    const computerChoice = opciones[Math.floor(Math.random() * opciones.length)];
+
+
+    document.getElementById("user-result").innerText = userChoice;
+    document.getElementById("computer-result").innerText = computerChoice;
+
+
+    if (userChoice === computerChoice) {
+        // Guardamos el texto en el HTML y hacemos el return
+        document.getElementById("game-status").innerText = "It's a draw!";
+        return `User choice ${userChoice}, computer choice ${computerChoice}, so, it's a draw!`;
+    }
+
+    if (rules[userChoice].includes(computerChoice)) {
+        document.getElementById("game-status").innerText = "You win!";
+        return `User choice ${userChoice}, computer choice ${computerChoice}, so, you win`;
+    } else {
+        document.getElementById("game-status").innerText = "You lose!";
+        return `User choice ${userChoice}, computer choice ${computerChoice}, so, you lose`;
+    }
+};
+
+
+document.getElementById("rock").addEventListener("click", () => jugar("rock"));
+document.getElementById("paper").addEventListener("click", () => jugar("paper"));
+document.getElementById("scissors").addEventListener("click", () => jugar("scissors"));
+document.getElementById("lizard").addEventListener("click", () => jugar("lizard"));
+document.getElementById("spock").addEventListener("click", () => jugar("spock"));
