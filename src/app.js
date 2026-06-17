ef5e65e5b1fff548ec0580df186ccb5c0c99c160
lexsaso <jalexanderssoto@hotmail.com>
2026-06-17 19:08:33 +0000

    arreglo app para que no falle fuera del navegador

--- src/app.js
+++ src/app.js
@@ -13,29 +13,35 @@ const jugar = (userChoice) => {
 
     const computerChoice = opciones[Math.floor(Math.random() * opciones.length)];
 
-
-    document.getElementById("user-result").innerText = userChoice;
-    document.getElementById("computer-result").innerText = computerChoice;
-
+    if (typeof document !== "undefined") {
+        document.getElementById("user-result").innerText = userChoice;
+        document.getElementById("computer-result").innerText = computerChoice;
+    }
 
     if (userChoice === computerChoice) {
-        // Guardamos el texto en el HTML y hacemos el return
-        document.getElementById("game-status").innerText = "It's a draw!";
+        if (typeof document !== "undefined") {
+            document.getElementById("game-status").innerText = "It's a draw!";
+        }
         return `User choice ${userChoice}, computer choice ${computerChoice}, so, it's a draw!`;
     }
 
     if (rules[userChoice].includes(computerChoice)) {
-        document.getElementById("game-status").innerText = "You win!";
+        if (typeof document !== "undefined") {
+            document.getElementById("game-status").innerText = "You win!";
+        }
         return `User choice ${userChoice}, computer choice ${computerChoice}, so, you win`;
     } else {
-        document.getElementById("game-status").innerText = "You lose!";
+        if (typeof document !== "undefined") {
+            document.getElementById("game-status").innerText = "You lose!";
+        }
         return `User choice ${userChoice}, computer choice ${computerChoice}, so, you lose`;
     }
 };
 
-
-document.getElementById("rock").addEventListener("click", () => jugar("rock"));
-document.getElementById("paper").addEventListener("click", () => jugar("paper"));
-document.getElementById("scissors").addEventListener("click", () => jugar("scissors"));
-document.getElementById("lizard").addEventListener("click", () => jugar("lizard"));
-document.getElementById("spock").addEventListener("click", () => jugar("spock"));
+if (typeof document !== "undefined") {
+    document.getElementById("rock").addEventListener("click", () => jugar("rock"));
+    document.getElementById("paper").addEventListener("click", () => jugar("paper"));
+    document.getElementById("scissors").addEventListener("click", () => jugar("scissors"));
+    document.getElementById("lizard").addEventListener("click", () => jugar("lizard"));
+    document.getElementById("spock").addEventListener("click", () => jugar("spock"));
+}
